ebc6d07bd15055cd9550e95c466677aad037f5c6
braincodeishan <braincodeishan@gmail.com>
2021-09-09 17:54:21 +0530

    11

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port =   3000;
-// process.env.PORT ||
+const port =  process.env.PORT || 3000;
+// sd
 const cors = require('cors');
 
 
